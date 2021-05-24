517cca7cbc8a87620c9564c0d4e63755f1b7b86d
choyoungkwang <choyoungkwang@nts-corp.com>
2021-05-24 14:49:09 +0900

    CSS Module 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
 import Customer from './components/Customer';
-import './App.css';
+import style from './App.module.css';
 
 const customers = [
   {
@@ -29,22 +29,23 @@ const customers = [
 
 const App = () => {  
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Id</TableCell>
-          <TableCell>Profile</TableCell>
-          <TableCell>Name</TableCell>
-          <TableCell>Birthday</TableCell>
-          <TableCell>Gender</TableCell>
-          <TableCell>Job</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        { customers.map(customer => <Customer key={customer.id} {...customer} />) }
-      </TableBody>
-    </Table>
-      
+    <div className={style.root}>
+      <Table className={style.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Profile</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Birthday</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Job</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { customers.map(customer => <Customer key={customer.id} {...customer} />) }
+        </TableBody>
+      </Table>
+    </div>
   )
 }
 
